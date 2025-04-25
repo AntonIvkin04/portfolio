3eebb84f2229539ef57e0878727096991f37004e
AntonIvkin04 <anton.ivkin54@gmail.com>
2025-04-25 22:13:04 +0200

    forms

--- src/app/shared/section-contact-me/section-contact-me.component.ts
+++ src/app/shared/section-contact-me/section-contact-me.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-section-contact-me',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './section-contact-me.component.html',
   styleUrl: './section-contact-me.component.css'
 })
 export class SectionContactMeComponent {
+  constructor() {
+
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      const formData = form.value;
+      console.log('Formulardaten:', formData);
+
+    } else {
+      console.log('Das Formular ist nicht gültig.');
+    }
+  }
 
 }

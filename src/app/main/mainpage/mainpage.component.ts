1ad41d4679104835ae63d63d3f16d6eedec1cdb3
AntonIvkin04 <anton.ivkin54@gmail.com>
2025-05-01 14:17:11 +0200

    footer section

--- src/app/main/mainpage/mainpage.component.ts
+++ src/app/main/mainpage/mainpage.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MainContentComponent } from '../main-content/main-content.component';
 import { SidebareComponent } from '../../shared/sidebare/sidebare.component';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [MainContentComponent, SidebareComponent, NavbarComponent],
+  imports: [MainContentComponent, SidebareComponent, NavbarComponent, FooterComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })
